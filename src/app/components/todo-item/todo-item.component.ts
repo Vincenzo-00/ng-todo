44477f77efcef36b7cfe2a24b983df780b16fb09
Vincenzo-00 <vince.vasta18@gmail.com>
2024-11-01 19:10:47 +0100

    @Output()

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,12 +1,21 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Todo } from '../../models/todo';
 
 @Component({
   selector: 'app-todo-item',
   templateUrl: './todo-item.component.html',
-  styleUrl: './todo-item.component.css'
+  styleUrl: './todo-item.component.css',
 })
 export class TodoItemComponent {
   @Input()
   todo?: Todo;
+
+  //  CREAZIONE DELL'ALLARME
+  @Output()
+  onRichiestaCompletamento = new EventEmitter<Todo>();
+
+  //  ATTIVAZIONE DELL'ALLARME
+  richiediCompletamento() {
+    this.onRichiestaCompletamento.emit(this.todo);
+  }
 }
