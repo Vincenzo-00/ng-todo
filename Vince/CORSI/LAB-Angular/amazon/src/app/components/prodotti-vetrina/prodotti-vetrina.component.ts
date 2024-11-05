c881c3a7a2cc5676a0acd54ecfba6909154899fa
Vincenzo-00 <vince.vasta18@gmail.com>
2024-11-05 17:10:07 +0100

    .

--- Vince/CORSI/LAB-Angular/amazon/src/app/components/prodotti-vetrina/prodotti-vetrina.component.ts
+++ Vince/CORSI/LAB-Angular/amazon/src/app/components/prodotti-vetrina/prodotti-vetrina.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProdottiService } from '../../services/prodotti.service';
+import { Product } from '../../models/prodotto';
 
 @Component({
   selector: 'app-prodotti-vetrina',
   templateUrl: './prodotti-vetrina.component.html',
   styleUrl: './prodotti-vetrina.component.css'
 })
-export class ProdottiVetrinaComponent {
+export class ProdottiVetrinaComponent implements OnInit {
+  prodotti: Product[] = [];
+
+  // dependency injection
+  //MECCANISMO CHE HA ANGULAR PER DARE A CHi NE HA BISOGNO 
+  //LA STESSA ISTANZA DI UN OGGETTO IN MODO TALE CHE SIA 
+  //CONDIVISA IN TUTTO IL PROGETTO
+  constructor(private prodottiService: ProdottiService) {
+    
+  } 
+  
+  ngOnInit(): void {
+    this.prodotti = this.prodottiService.getProdotti();    
+  }
 
 }
--- Vince/CORSI/LAB-Angular/amazon/src/app/services/prodotti.service.ts
+++ Vince/CORSI/LAB-Angular/amazon/src/app/services/prodotti.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { PRODOTTI } from "../data/prodotti";
+import { Product } from '../models/prodotto';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,9 @@ import { Injectable } from '@angular/core';
 export class ProdottiService {
 
   constructor() { }
+
+  getProdotti() {
+    return PRODOTTI as Product[];
+  }
+
 }
